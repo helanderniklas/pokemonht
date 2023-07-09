0fb74fdfec5b8a0bef4f5dbe944e9bc31ed64880
Mirooo <kymalainen.samuel@gmail.com>
2023-07-09 17:57:25 +0300

    added scripts for MainActivity buttons

--- app/src/main/java/com/example/harkkatyo/MainActivity.java
+++ app/src/main/java/com/example/harkkatyo/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.harkkatyo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button gotoadd = findViewById(R.id.addpkm);
+        Button gotolist = findViewById(R.id.listpkm);
+        Button gotomove = findViewById(R.id.movepkm);
+        Button gotofight = findViewById(R.id.gotofight);
+    }
+
+    public void SwitchToAdd(View view){
+        Intent intent = new Intent(this, AddPokemons.class);
+        startActivity(intent);
+    }
+
+    public void SwitchToList(View view){
+        Intent intent = new Intent(this, AddPokemons.class);
+        startActivity(intent);
+    }
+
+    public void SwitchToMove(View view){
+        Intent intent = new Intent(this, AddPokemons.class);
+        startActivity(intent);
+    }
+
+    public void SwitchToFight(View view){
+        Intent intent = new Intent(this, AddPokemons.class);
+        startActivity(intent);
     }
 }
